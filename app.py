aed08b729b5d95bc9bfbbe0f031c91f5eb3531c3
Your Name <your-email@example.com>
2025-08-06 13:23:48 -0400

    checking similarity between title and image vs description and image

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from PIL import Image
 import requests
 from io import BytesIO
 from trutext import truncate_description
+from similarity import cosine_similarity
+
 
 app = Flask(__name__)
 CORS(app)  # <-- Enables CORS for all domains and routes
@@ -19,13 +21,10 @@ model, preprocess = clip.load("ViT-B/32", device=device)
 @app.route("/receive-data", methods=["POST"])
 def receive_data():
     data = request.get_json()
-    #print("received data", data)
+    print("received data", data)
 
     image_url = data.get("imageUrl")
     description = data.get("description")
-
-    #print("📦 imageUrl:", image_url)
-    #print("📦 description:", description)
 
     if not image_url or not description:
         print("❌ Missing required fields.")
@@ -41,30 +40,38 @@ def receive_data():
         # Generate embeddings
     image_input = preprocess(image).unsqueeze(0).to(device)
 
-    truncated = truncate_description(description)
-    text_input = clip.tokenize(truncated).to(device)
+    title = data.get("title", "")
+    description = data.get("description", "")
+    truncated_desc = truncate_description(description)
+    truncated_title = truncate_description(title)
+
+    desc_input = clip.tokenize(truncated_desc).to(device)
+    title_input = clip.tokenize(truncated_title).to(device)
 
     with torch.no_grad():
-        #print("encoding image")
         image_features = model.encode_image(image_input)
-        #print("encoded image")
-
-        #print("encoding text")
-        text_features = model.encode_text(text_input)
-        #print("encoded text")
+        desc_features = model.encode_text(desc_input)
+        title_features = model.encode_text(title_input)
 
 
     # Convert to list for JSON serialization
-    image_embedding = image_features.cpu().numpy().tolist()[0]
-    text_embedding = text_features.cpu().numpy().tolist()[0]
+    similarity_desc = cosine_similarity(
+        image_features.cpu().numpy().tolist()[0],
+        desc_features.cpu().numpy().tolist()[0]
+    )
 
-    print("image embedding", image_embedding)
-    print("text embedding", text_embedding)
+    similarity_title = cosine_similarity(
+        image_features.cpu().numpy().tolist()[0],
+        title_features.cpu().numpy().tolist()[0]
+    )
 
     return jsonify({
-        "image_embedding": image_embedding,
-        "text_embedding": text_embedding
+        "similarity_description": similarity_desc,
+        "similarity_title": similarity_title
     })
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
